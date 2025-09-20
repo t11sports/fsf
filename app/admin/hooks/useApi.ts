17630e029f50e2c4bd3c03fe0b10a1803d4e7ccf
tredeezy11 <strahan.ucm@gmail.com>
2025-09-19 18:46:09 -0700

    Update useApi.ts

--- app/admin/hooks/useApi.ts
+++ app/admin/hooks/useApi.ts
@@ -1,12 +1,16 @@
 import axios from 'axios';
+type Buyer = {
+  name: string;
+  email: string;
+};
 
 export const useApi = () => {
   const fetchBuyers = async () => {
     const res = await axios.get('/api/buyers');
-    return res.data;
+    return res.data as Buyer[];
   };
 
-  const createBuyer = async (data) => {
+  const createBuyer = async (data: Buyer) => {
     const res = await axios.post('/api/buyers', data);
     return res.data;
   };
