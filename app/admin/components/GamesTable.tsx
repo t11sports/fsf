22d12e1e4aedd2b37bf1303d743f7e628434a03f
tredeezy11 <strahan.ucm@gmail.com>
2025-09-19 18:21:20 -0700

    Create GamesTable.tsx

---
+++ app/admin/components/GamesTable.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const GamesTable = () => {
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/games').then(res => setGames(res.data));
+  }, []);
+
+  return (
+    <table className="w-full mt-4 border text-sm">
+      <thead className="bg-gray-100">
+        <tr>
+          <th className="p-2 border">Week</th>
+          <th className="p-2 border">Home Team</th>
+          <th className="p-2 border">Away Team</th>
+          <th className="p-2 border">Date</th>
+        </tr>
+      </thead>
+      <tbody>
+        {games.map((game, idx) => (
+          <tr key={idx} className="hover:bg-gray-50">
+            <td className="p-2 border">{game.week}</td>
+            <td className="p-2 border">{game.home}</td>
+            <td className="p-2 border">{game.away}</td>
+            <td className="p-2 border">{game.date}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default GamesTable;
