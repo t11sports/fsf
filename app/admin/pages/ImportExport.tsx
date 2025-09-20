1ac028440dcde285cdc735707ca08df38af84796
tredeezy11 <strahan.ucm@gmail.com>
2025-09-19 17:53:08 -0700

    Create ImportExport.tsx

---
+++ app/admin/pages/ImportExport.tsx
@@ -0,0 +1,11 @@
+const ImportExport = () => (
+  <div className="text-xl font-bold">
+    Import / Export Tools
+    <ul className="mt-4 list-disc ml-6 text-base">
+      <li>Import from CSV</li>
+      <li>Export to CSV</li>
+      <li>Batch upload management</li>
+    </ul>
+  </div>
+);
+export default ImportExport;
